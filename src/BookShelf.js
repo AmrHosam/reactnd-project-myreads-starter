e222ad8cc6913842f97900dfd94d0dc71fd62b91
AmrHosam <amrhossam97@gmail.com>
2021-05-09 20:38:43 +0200

    feat: Add Book component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Book from './Book'
 
 class BookShelf extends Component {
     render() {
@@ -6,27 +7,8 @@ class BookShelf extends Component {
         return(
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                    {showingBooks.map((book,index) => (
-                        <li key={index}>
-                            <div className="book">
-                                <div className="book-top">
-                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.thumbnail})` }}></div>
-                                  <div className="book-shelf-changer">
-                                    <select>
-                                      <option value="move" disabled>Move to...</option>
-                                      <option value="currentlyReading">Currently Reading</option>
-                                      <option value="wantToRead">Want to Read</option>
-                                      <option value="read">Read</option>
-                                      <option value="none">None</option>
-                                    </select>
-                                  </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                {book.authors.map((author, index) => (
-                                  <div className="book-authors" key={index}>{author}</div>
-                                  ))}
-                            </div>
-                        </li>
+                    {showingBooks.map((book) => (
+                        <Book key={book.id} book={book}/>
                     ))}
                 </ol>
             </div>
