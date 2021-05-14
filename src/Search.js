4753407e19b5c9052a09cf0e4306b58036a6e25a
AmrHosam <amrhossam97@gmail.com>
2021-05-14 03:01:35 +0200

    feat: Search for books on input change

--- src/Search.js
+++ src/Search.js
@@ -9,6 +9,33 @@ class Search extends Component {
     updateQuery = (event) => {
         event.preventDefault();
         this.setState({ query: event.target.value });
+        const { books } = this.props;
+        const showingResults = []
+        if(event.target.value !== "") {
+            BooksAPI.search(event.target.value).then((results) => {
+                if(Array.isArray(results)) {
+                    Object.keys(results).map((id) => {
+                        const foundBook = books.find((book) => (book.id === results[id].id))
+                        if(foundBook) {
+                            showingResults.push(foundBook)
+                        } else {
+                            const newBook = {
+                                id: results[id].id,
+                                title: results[id].title,
+                                authors: results[id].authors,
+                                shelf: "none",
+                                thumbnail: results[id].imageLinks.thumbnail
+                            }
+                            showingResults.push(newBook)
+                        }
+
+                    })
+                }
+                this.setState({showingResults: showingResults})
+            })
+        } else {
+            this.setState({showingResults: []})
+        }
     }
     render() {
         return(
